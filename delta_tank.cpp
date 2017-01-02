34528e5e7b21f6873f6630f89f27ed91fe5e0936
李思隆 <923631911@qq.com>
2017-01-02 17:53:09 +0800

    Update delta_tank.cpp

--- delta_tank.cpp
+++ delta_tank.cpp
@@ -1,5 +1,3 @@
-// delta_test.cpp : 定义控制台应用程序的入口点。
-
 #include "stdafx.h"
 #include <iostream>
 
@@ -51,13 +49,11 @@ public:
 
 		//Must start moving first
 		if(keyboard->GetKeyState(osgGA::GUIEventAdapter::KEY_Up)||keyboard->GetKeyState(osgGA::GUIEventAdapter::KEY_Down)){
-		//if(kc==osgGA::GUIEventAdapter::KEY_Up||kc==osgGA::GUIEventAdapter::KEY_Down){
-			
+
 			model->SetEnabled(true);
 			
 			//Left Ctrl to accelerate, maximun speed 40.0f
 			if(keyboard->GetKeyState(osgGA::GUIEventAdapter::KEY_Control_L)){
-			//if(kc==osgGA::GUIEventAdapter::KEY_Control_L){
 				if(speed<30.0f){
 					speed += 0.5f;
 					SetWalkMode(brdm,speed);
@@ -65,7 +61,6 @@ public:
 			}
 			//Left Shift to decelerate, minimum speed 6.0f 
 			if(keyboard->GetKeyState(osgGA::GUIEventAdapter::KEY_Shift_L)){
-			//if(kc==osgGA::GUIEventAdapter::KEY_Shift_L){
 				if(speed>6.0f){
 					speed -= 0.5f;
 					SetWalkMode(brdm,speed);
@@ -119,7 +114,7 @@ public:
 		AddDrawable(brdm.get());
 
 		speed = 6.0f;
-		k=0;
+		
 		//Add map
 		flatdirt = new Object("flatdirt");
 		flatdirt->LoadFile("../module/flatdirt.ive");
@@ -129,33 +124,6 @@ public:
 		osg::Vec3 fd1Rotation(0.0f,0.0f,0.0f);
 		trans_1.SetTranslation(fd1Position);
 		flatdirt->SetTransform(trans_1);
-		
-		flatdirt_2 = new Object("flatdirt");
-		flatdirt_2->LoadFile("../module/flatdirt.ive");
-		AddDrawable(flatdirt_2.get());
-		dtCore::Transform trans_2;
-		osg::Vec3 fd2Position(0.0f,40.0f,0.0f);
-		osg::Vec3 fd2Rotation(0.0f,0.0f,0.0f);
-		trans_2.SetTranslation(fd2Position);
-		flatdirt_2->SetTransform(trans_2);
-
-		flatdirt_3 = new Object("flatdirt");
-		flatdirt_3->LoadFile("../module/flatdirt.ive");
-		AddDrawable(flatdirt_3.get());
-		dtCore::Transform trans_3;
-		osg::Vec3 fd3Position(20.0f,40.0f,0.0f);
-		osg::Vec3 fd3Rotation(0.0f,0.0f,0.0f);
-		trans_3.SetTranslation(fd3Position);
-		flatdirt_3->SetTransform(trans_3);
-		
-		flatdirt_4 = new Object("flatdirt");
-		flatdirt_4->LoadFile("../module/flatdirt.ive");
-		AddDrawable(flatdirt_4.get());
-		dtCore::Transform trans_4;
-		osg::Vec3 fd4Position(-20.0f,40.0f,0.0f);
-		osg::Vec3 fd4Rotation(0.0f,0.0f,0.0f);
-		trans_4.SetTranslation(fd4Position);
-		flatdirt_4->SetTransform(trans_4);
 
 		//Set tank position
 		osg::Vec3 brdmPosition(0.0f,-25.0f,0.0f);
@@ -189,14 +157,10 @@ public:
 
 private:
 	RefPtr<Object> flatdirt;
-	RefPtr<Object> flatdirt_2;
-	RefPtr<Object> flatdirt_3;
-	RefPtr<Object> flatdirt_4;
 	RefPtr<Object> brdm;
 	RefPtr<dtCore::WalkMotionModel> model;
 	Sound* _sound;
 	float speed;
-	int k;
 };
 
 int main()
@@ -204,7 +168,6 @@ int main()
 	App app;
 	app.Config();
 	app.Run();
-
 	cout<<"program end"<<endl;
 	return 0;
 	
